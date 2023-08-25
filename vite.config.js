42bc7fc8790675cbd2fb57ef7b93ca196075e7f1
tinymelon <e.pratusevich@gmail.com>
2023-08-25 23:58:09 +0300

    Vite rollupOptions added for multiple entry points

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,20 @@ import handlebars from 'vite-plugin-handlebars';
 export default defineConfig({
     root: resolve(__dirname, 'src'),
     build: {
-        outDir: resolve(__dirname, 'dist')
+        outDir: resolve(__dirname, 'dist'),
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'src', 'index.html'),
+                reg: resolve(__dirname, 'src', 'registration.html'),
+                profile: resolve(__dirname, 'src', 'profile.html'),
+                profileEd: resolve(__dirname, 'src', 'profile_edit.html'),
+                profilePas: resolve(__dirname, 'src', 'profile_password.html'),
+                err404: resolve(__dirname, 'src', 'error404.html'),
+                err500: resolve(__dirname, 'src', 'error500.html'),
+            },
+        },
     },
+    appType: 'mpa',
     plugins: [handlebars({
         partialDirectory: resolve(__dirname, 'src/partials')
     })],
