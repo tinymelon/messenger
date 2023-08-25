4c8d6524410b3d4170dca4f2c418bf0ffcdbc919
tinymelon <e.pratusevich@gmail.com>
2023-08-25 15:35:13 +0300

    Express server added

---
+++ server.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const app = express();
+const PORT = 3000;
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
+
+app.use(express.static(path.resolve(__dirname, 'dist')));
+
+app.listen(PORT, function () {
+    console.log(`Example app listening on port ${PORT}!`);
+});
